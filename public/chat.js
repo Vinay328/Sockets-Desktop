44e1d232d011c854707ed26e78cef6fdeeccd0ee
Vinay328 <vinay.k2233@gmail.com>
2018-08-29 15:46:55 +0530

    Master -> Electron changes

---
+++ public/chat.js
@@ -0,0 +1,50 @@
+
+let ipc = require('electron').ipcRenderer;
+
+// query dom
+const message = document.getElementById('message');
+const sendMessageButton = document.getElementById('send-message');
+const writeContent = document.getElementById('write-content');
+const submitBtn = document.getElementById('username-submit');
+const overlay = document.getElementById('overlay');
+
+let username;
+
+submitBtn.addEventListener('click', function () {
+
+    if(document.getElementById('user-name').value.trim()){
+        username = document.getElementById('user-name').value;
+        overlay.style.display = "none";
+        ipc.send('capture-username', username);
+    }else{
+        alert("please enter user name");
+    }
+
+});
+
+//Send user message
+sendMessageButton.addEventListener('click', function () {
+
+    if(message.value.trim()) {
+
+        let data = {
+            message: message.value,
+            userName: username
+        };
+
+        writeContent.innerHTML += "<p><strong>"+ data.userName + ':</strong>' + data.message + '</p>';
+        ipc.send('emit-message', data);
+    } else {
+        alert("please enter your message");
+    }
+
+});
+
+//On receiving the message write the content to the block
+ipc.on('message-received', function (events, data) {
+    writeContent.innerHTML += "<p><strong>"+ data.userName + ':</strong>' + data.message + '</p>';
+});
+
+ipc.on('own-message', function (events, data) {
+    writeContent.innerHTML += "<p><strong>"+ data.userName + ':</strong>' + data.message + '</p>';
+});
